4f4d5d5e8b65ded17419dedd8b9e5ffb2dd4b669
DanielKatt <pinzondiazdaniel@gmail.com>
2022-03-25 15:48:28 -0500

    Push

--- client/src/components/welcomeCard/WelcomeCard.jsx
+++ client/src/components/welcomeCard/WelcomeCard.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import '../welcomeCard/welcomeCard.css'
+import styles from './welcomeCard.module.css'
 
 const WelcomeCard = () => {
     return (
-        <div>   
-            <div className="container">
-              <div className="back">
-                <div className="card">
-                  <div className="box">
-                    <div className="content">
-                      <h3>Querido/a estudiante</h3>
-                      <ul className="list">
+        <div>  
+          <div className={styles.containerOne}>
+            <div className={styles.progressContainer}>
+                <div className={styles.progress}> 
+                  <div className={styles.circle.active}>1</div>
+                  <div className={styles.circle}>2</div>
+                  <div className={styles.circle}>3</div>
+              </div>
+            </div>
+          </div>
+            <div className={styles.container}>
+              <div className={styles.back}>
+                <div className={styles.card}>
+                  <div className={styles.box}>
+                    <div className={styles.content}>
+                    <h3>Querido/a estudiante</h3>
+                      <ul className={styles.list}>
+
                           <li>Gracias por realizar la inscripción.</li>
                           <li>No olvides completar los 3 pasos para empezar tu proceso de mentoría.</li>
                           <li>Puedes contactar con tus formadores, si tienes alguna duda.</li>
                       </ul>
-                      <a href="#n" className="next">Siguiente</a>
+                      <a href="#n" className={styles.btn}>Siguiente</a>
                     </div>              
                   </div>
                   {/* <h1 className="little">.hola.</h1> */}
